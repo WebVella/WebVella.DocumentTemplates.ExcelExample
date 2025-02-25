0b3b550c9ffd75666414daae4a5061626cd793cd
Bozhidar Zashev <bzashev@users.noreply.github.com>
2025-02-25 13:18:55 +0200

    initial

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Data;
+using WebVella.DocumentTemplates.Engines.ExcelFile;
+
+namespace WebVella.DocumentTemplates.ExcelExample
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var templateFile = "TemplateDoc1.xlsx";
+			var ds = new DataTable();
+			ds.Columns.Add("position", typeof(int));
+			ds.Columns.Add("sku", typeof(string));
+			ds.Columns.Add("item", typeof(string));
+			ds.Columns.Add("price", typeof(decimal));
+
+			for (int i = 1; i < 6; i++)
+			{
+				var dsrow = ds.NewRow();
+				dsrow["position"] = i;
+				dsrow["sku"] = $"SKU{i}";
+				dsrow["item"] = $"item {i} description text";
+				dsrow["price"] = i * (decimal)0.98;
+				ds.Rows.Add(dsrow);
+			}
+
+			var template = new WvExcelFileTemplate
+			{
+				Template = new Utils().LoadWorkbookAsMemoryStream(templateFile)
+			};
+			WvExcelFileTemplateProcessResult? result = template.Process(ds);
+
+			new Utils().SaveWorkbookFromMemoryStream(result.ResultItems[0].Result!, "TemplateDoc1.xlsx");
+		}
+
+
+	}
+}
---
+++ Utils.cs
@@ -0,0 +1,33 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebVella.DocumentTemplates.ExcelExample;
+public class Utils
+{
+	public MemoryStream? LoadWorkbookAsMemoryStream(string fileName)
+	{
+		var path = Path.Combine(Environment.CurrentDirectory, $"{fileName}");
+		var fi = new FileInfo(path);
+		var templateWB = new XLWorkbook(path);
+		MemoryStream? ms = new();
+		templateWB.SaveAs(ms);
+		return ms;
+	}
+	public void SaveWorkbookFromMemoryStream(MemoryStream content, string fileName)
+	{
+		DirectoryInfo? debugFolder = Directory.GetParent(Environment.CurrentDirectory);
+		if (debugFolder is null) throw new Exception("debugFolder not found");
+		var projectFolder = debugFolder.Parent?.Parent;
+		if (projectFolder is null) throw new Exception("projectFolder not found");
+
+		var path = Path.Combine(projectFolder.FullName, $"result-{fileName}");
+		using (FileStream fs = new FileStream(path, FileMode.Create))
+		{
+			content.WriteTo(fs);
+		}
+	}
+}
